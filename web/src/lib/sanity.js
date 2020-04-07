19f0967252d9e18a8e98ae6564255351c1bb62e9
Knut Melvær <knut.melvaer@gmail.com>
2020-04-07 21:43:58 +0200

    Remove token

--- web/src/lib/sanity.js
+++ web/src/lib/sanity.js
@@ -3,8 +3,6 @@ const client = sanityClient({
   projectId: 'nozluz6d',
   dataset: 'production',
   useCdn: true,
-  token:
-    'skKZs1WEJbLj62uWng9yQj3LTTGVS6hEdgPqB1V3GZ68wuVbVjCEEStaupLUA3L4pTs5DM9cuY19OQTq8uhweBd4boAyC9y6aAytIL2CR5llRHXUGbGI2h0RJ72TQEOXrRWi7XoRaDGE81qtqH62EKrKtDYB8z01hq1MGw13MIK9v6sdRIzh'
 });
 
 export default client;
